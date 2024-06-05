ad0cc3bf866d671b63a855aa2216390f1bcbfe9b
47vigen <47vigen@gmail.com>
2024-06-05 17:43:57 +0330

    feat: initialize

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  tabWidth: 2,
+  endOfLine: "lf",
+  singleQuote: true,
+  trailingComma: "es5"
+}
+
+export default config
